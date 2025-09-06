543b2d681df2ede5df5b15b5649ba54581dd333c
TuyuriTio <tuyuritio@gmail.com>
2025-09-07 08:11:29 +0900

    fix(style): adjust navigator layout

--- src/components/header/Navigator.svelte
+++ src/components/header/Navigator.svelte
@@ -81,6 +81,9 @@
 						position: static;
 						width: auto;
 
+						display: inline-flex;
+						align-items: center;
+
 						border-bottom: none;
 						color: var(--primary-color);
 					}
@@ -114,7 +117,7 @@
 <div role="button" onclick={() => (menu = false)} class:pointer-events-none={!menu} class:bg-transparent={!menu} class="fixed top-0 left-0 w-screen h-screen pointer-events-auto bg-#aaaaaa88 transition-background-color sm:hidden"></div>
 
 <nav bind:this={navigator} class:transform-translate-x-full={!menu} class="fixed top-0 right-0 flex flex-col justify-between items-start gap-5 p-5 bg-background h-full sm:contents overflow-hidden transition-transform">
-	<header class="grid gap-5 c-secondary grid-rows-[repeat(5,1fr)] sm:(grid-rows-none grid-cols-[repeat(5,1fr)])">
+	<header class="grid gap-5 c-secondary grid-rows-[repeat(5,1fr)] sm:(grid-rows-none grid-cols-[repeat(4,1fr)])">
 		<button onclick={() => (menu = false)} class="sm:hidden">{@render close()}</button>
 
 		<a href={getRelativeLocaleUrl(locale)} class:location={route == getRelativeLocaleUrl(locale) || route.startsWith(getRelativeLocaleUrl(locale, "/preface"))}>
@@ -161,7 +164,7 @@
 	import ThemeSwitcher from "./ThemeSwitcher.svelte";
 	import Menu from "./Menu.svelte";
 
-	let { locale, route, home, note, jotting, about, globe, rss, sun, moon, bars, close }: { locale: string; route: string; home: Snippet; note: Snippet; jotting: Snippet; about: Snippet; globe: Snippet; rss: Snippet; sun: Snippet; moon: Snippet; bars: Snippet; close: Snippet } = $props();
+	let { locale, route, home, note, jotting, about, globe, rss, sun, moon, bars, close }: { locale: string; route: string } & { [key: string]: Snippet } = $props();
 
 	const t = i18nit(locale);
 
