dbe643043213d8476834b6c34c4e37833347c012
Вадим Христенко <vadim.khristenko@gmail.com>
2025-10-05 23:55:28 +0300

    Add attrs import to astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -30,6 +30,7 @@ import sectionize from "@hbsnow/rehype-sectionize";
 
 import spoiler from "./src/utils/remark/spoiler";
 import abbr from "./src/utils/remark/abbr";
+import attrs from "./src/utils/remark/attrs";
 import wrapper from "./src/utils/remark/table-wrapper";
 import copy from "./src/utils/code-copy";
 import reading from "./src/utils/remark/reading";
@@ -61,6 +62,7 @@ export default defineConfig({
       gemoji,
       footnote,
       abbr,
+      attrs,
       wrapper,
       [table, { colspanWithEmpty: true }],
       wrapper,
