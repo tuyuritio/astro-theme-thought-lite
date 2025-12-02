c3e753b507138910a45e08f6b82f7969780a3821
TuyuriTio <tuyuritio@gmail.com>
2025-12-03 03:44:43 +0900

    refactor(style)!: migrate from UnoCSS to Tailwind CSS v4
    Due to breaking updates of UnoCSS (see #4977), this commit fully migrates the project to Tailwind CSS v4.

--- src/layouts/header/Menu.svelte
+++ src/layouts/header/Menu.svelte
@@ -4,10 +4,10 @@ import { type Snippet } from "svelte";
 let { label, trigger, children }: { label?: string; trigger: Snippet; children: Snippet } = $props();
 </script>
 
-<div class="flex flex-col justify-center b-r b-r-solid pr-2 b-primary sm:(relative b-0 p-0) group">
-	<button class="items-center sm:(b-none)" aria-label={label}>{@render trigger()}</button>
-	<div class="pt-1 z-1 bg-background sm:(absolute top-full left-1/2 h-max transform-translate-x--1/2 opacity-0 invisible transition-[opacity,visibility]) sm:group-hover:(opacity-100 visible)">
-		<div class="flex flex-col sm:(b-2 b-solid b-primary shadow-md)">
+<div class="flex flex-col justify-center border-r border-r-solid pr-2 border-primary sm:relative sm:border-0 sm:p-0 group">
+	<button class="items-center sm:border-none" aria-label={label}>{@render trigger()}</button>
+	<div class="pt-1 z-1 bg-background sm:absolute sm:top-full sm:left-1/2 sm:h-max sm:-translate-x-1/2 sm:opacity-0 sm:invisible sm:transition-[opacity,visibility] sm:group-hover:opacity-100 sm:group-hover:visible">
+		<div class="flex flex-col sm:border-2 sm:border-solid sm:border-primary sm:shadow-md">
 			{@render children()}
 		</div>
 	</div>
