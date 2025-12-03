f2ce3841673dce685f8bd7d794086a3246c7ec23
TuyuriTio <tuyuritio@gmail.com>
2025-12-04 01:03:54 +0900

    style: globalize menu item style

--- src/layouts/header/Menu.svelte
+++ src/layouts/header/Menu.svelte
@@ -7,8 +7,35 @@ let { label, trigger, children }: { label?: string; trigger: Snippet; children: 
 <div class="flex flex-col justify-center border-r pr-2 border-primary sm:relative sm:border-0 sm:p-0 group">
 	<button class="items-center sm:border-none" aria-label={label}>{@render trigger()}</button>
 	<div class="pt-1 z-1 bg-background sm:absolute sm:top-full sm:left-1/2 sm:h-max sm:-translate-x-1/2 sm:opacity-0 sm:invisible sm:transition-[opacity,visibility] sm:group-hover:opacity-100 sm:group-hover:visible">
-		<div class="flex flex-col sm:border-2 sm:border-primary sm:shadow-md">
+		<div id="menu" class="flex flex-col sm:border-2 sm:border-primary sm:shadow-md">
 			{@render children()}
 		</div>
 	</div>
 </div>
+
+<style>
+	#menu :global(a) {
+		font-size: 0.875rem;
+		line-height: 1.15rem;
+		white-space: nowrap;
+
+		padding: 0.125rem 0;
+
+		@media (min-width: 640px) {
+			& {
+				padding: 0.5rem 0.75rem;
+
+				font-weight: bold;
+
+				transition:
+					color 150ms ease-in-out,
+					background-color 150ms ease-out;
+
+				&:hover {
+					color: var(--background-color);
+					background-color: var(--primary-color);
+				}
+			}
+		}
+	}
+</style>
