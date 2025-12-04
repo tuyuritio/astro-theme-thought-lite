2d459791f11c20ef10129b4981a5357966095482
TuyuriTio <tuyuritio@gmail.com>
2025-12-05 04:21:13 +0900

    fix: move menu ID to class attribute

--- src/layouts/header/Menu.svelte
+++ src/layouts/header/Menu.svelte
@@ -7,14 +7,14 @@ let { label, trigger, children }: { label?: string; trigger: Snippet; children: 
 <div class="flex flex-col justify-center border-r pr-2 border-primary sm:relative sm:border-0 sm:p-0 group">
 	<button class="items-center sm:border-none" aria-label={label}>{@render trigger()}</button>
 	<div class="pt-1 z-1 bg-background sm:absolute sm:top-full sm:left-1/2 sm:h-max sm:-translate-x-1/2 sm:opacity-0 sm:invisible sm:transition-[opacity,visibility] sm:group-hover:opacity-100 sm:group-hover:visible">
-		<div id="menu" class="flex flex-col sm:border-2 sm:border-primary sm:shadow-md">
+		<div class="menu flex flex-col sm:border-2 sm:border-primary sm:shadow-md">
 			{@render children()}
 		</div>
 	</div>
 </div>
 
 <style>
-	#menu :global(a) {
+	.menu :global(a) {
 		font-size: 0.875rem;
 		line-height: 1.15rem;
 		white-space: nowrap;
