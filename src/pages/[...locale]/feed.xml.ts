cccde9cdcd54745128588fb1c92097b181555e30
TuyuriTio <tuyuritio@gmail.com>
2025-12-15 20:57:43 +0900

    fix(feed): use absolute path for stylesheet reference

--- src/pages/[...locale]/feed.xml.ts
+++ src/pages/[...locale]/feed.xml.ts
@@ -112,7 +112,7 @@ export const GET: APIRoute = async ({ site, params }) => {
 	});
 
 	// Append stylesheet declaration to the feed
-	const xml = feed.atom1().replace(/(<\?xml version="1\.0" encoding="utf-8".*\?>)/, '$1\n<?xml-stylesheet type="text/xsl" href="feed.xsl"?>');
+	const xml = feed.atom1().replace(/(<\?xml version="1\.0" encoding="utf-8".*\?>)/, '$1\n<?xml-stylesheet type="text/xsl" href="/feed.xsl"?>');
 
 	return new Response(xml, { headers: { "Content-Type": "application/xml" } });
 };
