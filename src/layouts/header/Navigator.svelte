b26702df094913a40c882493f6b047e1416c833f
TuyuriTio <tuyuritio@gmail.com>
2025-12-23 20:35:53 +0900

    style: bold active language link in menu

--- src/layouts/header/Navigator.svelte
+++ src/layouts/header/Navigator.svelte
@@ -87,7 +87,7 @@ onMount(() => {
 				{#snippet trigger()}<Icon name="lucide--earth" />{/snippet}
 				<div data-no-swup class="contents">
 					{#each config.i18n.locales as target}
-						<a href={locale === target ? undefined : getRelativeLocaleUrl(target as string, path)} lang={target} class={locale === target ? "bg-primary text-background" : ""}>{i18nit(target)("language")}</a>
+						<a href={locale === target ? undefined : getRelativeLocaleUrl(target as string, path)} lang={target} class={locale === target ? "font-bold sm:bg-primary sm:text-background" : ""}>{i18nit(target)("language")}</a>
 					{/each}
 				</div>
 			</Menu>
