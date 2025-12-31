693c9cd70471ffe8a81f5e15961e4aeee26b59c6
TuyuriTio <tuyuritio@gmail.com>
2025-12-31 14:50:37 +0800

    fix(graph): enhance Open Graph image generation with fonts caching

--- src/pages/[...locale]/jotting/[...id]/graph.png.ts
+++ src/pages/[...locale]/jotting/[...id]/graph.png.ts
@@ -1,7 +1,8 @@
 import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 import config, { monolocale } from "$config";
-import graph from "$graph/content";
+import graph, { contentMap } from "$graph/content";
+import i18nit from "$i18n";
 
 export async function getStaticPaths() {
 	const jottings = await getCollection("jotting", jotting => !jotting.data.draft);
@@ -19,13 +20,17 @@ export async function getStaticPaths() {
 			id = ids.join("/");
 		}
 
+		contentMap.set(jotting.id, {
+			locale: locale || config.i18n.defaultLocale,
+			type: i18nit(locale || config.i18n.defaultLocale)("navigation.jotting"),
+			title: jotting.data.title,
+			timestamp: jotting.data.timestamp.toISOString().split("T")[0].replace(/-/g, "/"),
+			tags: jotting.data.tags
+		});
+
 		return {
 			params: { locale, id },
-			props: {
-				title: jotting.data.title,
-				timestamp: jotting.data.timestamp,
-				tags: jotting.data.tags
-			}
+			props: { id: jotting.id }
 		};
 	});
 }
@@ -33,16 +38,8 @@ export async function getStaticPaths() {
 /**
  * GET handler that generates and returns the Open Graph image for a jotting.
  */
-export const GET: APIRoute = async ({ params, props }) => {
-	const image = await graph({
-		locale: params.locale || config.i18n.defaultLocale,
-		type: "jotting",
-		site: config.title,
-		author: config.author.name,
-		title: props.title,
-		timestamp: props.timestamp,
-		tags: props.tags
-	});
+export const GET: APIRoute = async ({ props }) => {
+	const image = await graph(props.id);
 
 	return new Response(new Uint8Array(image), { headers: { "Content-Type": "image/png" } });
 };
