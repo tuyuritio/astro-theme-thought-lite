e4302f52ce5ba0ac4064df44cb8c7233ffe997f6
TuyuriTio <tuyuritio@gmail.com>
2026-01-16 02:42:44 +0900

    chore: upgrade dependency and refactor ZeoSevenFonts provider

--- astro.config.ts
+++ astro.config.ts
@@ -34,7 +34,7 @@ import reading from "./src/utils/remark/reading";
 import figure from "./src/utils/remark/figure";
 
 import siteConfig from "./site.config";
-import { ZeoSevenFonts } from "./src/fonts/config";
+import ZeoSevenFonts from "./src/fonts/zeo-seven-fonts";
 
 // https://astro.build/config
 export default defineConfig({
@@ -145,6 +145,7 @@ export default defineConfig({
 				provider: fontProviders.google(),
 				weights: [100],
 				display: "block",
+				subsets: ["fallback"],
 				fallbacks: ["serif"],
 				cssVariable: "--font-playwrite-mx"
 			},
--- src/fonts/config.ts
+++
@@ -1,7 +0,0 @@
-import { defineAstroFontProvider } from "astro/config";
-
-export function ZeoSevenFonts() {
-	return defineAstroFontProvider({
-		entrypoint: "src/fonts/zeo_seven_fonts.ts"
-	});
-}
