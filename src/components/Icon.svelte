0c1f96c440a599c02d7a34ff4b518f5cfcf9d03f
TuyuriTio <tuyuritio@gmail.com>
2026-02-07 05:46:53 +0900

    refactor: establish global z-index hierarchy

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -23,7 +23,7 @@ let dimensions = value ? `width: ${value}; height: ${value}` : "";
 	<span class="iconify {name} {className}" style={dimensions} aria-hidden="true"></span>
 
 	{#if title}
-		<span class="absolute bottom-full start-1/2 w-max h-max mb-1 py-1 px-1.5 rounded-sm text-background bg-primary text-xs font-serif font-normal leading-tight whitespace-pre-line z-2 opacity-0 invisible -translate-x-1/2 rtl:translate-x-1/2 translate-y-1 pointer-events-none group-hover/icon:opacity-100 group-hover/icon:visible group-hover/icon:translate-y-0 transition-[opacity,visibility,translate] duration-150">
+		<span class="absolute bottom-full start-1/2 w-max h-max mb-1 py-1 px-1.5 rounded-sm text-background bg-primary text-xs font-serif font-normal leading-tight whitespace-pre-line z-10 opacity-0 invisible -translate-x-1/2 rtl:translate-x-1/2 translate-y-1 pointer-events-none group-hover/icon:opacity-100 group-hover/icon:visible group-hover/icon:translate-y-0 transition-[opacity,visibility,translate] duration-150">
 			{title}
 		</span>
 	{/if}
--- src/layouts/header/Menu.svelte
+++ src/layouts/header/Menu.svelte
@@ -6,7 +6,7 @@ let { label, trigger, children }: { label?: string; trigger: Snippet; children: 
 
 <div class="flex flex-col justify-center border-e pe-2 border-primary sm:relative sm:border-0 sm:p-0 group">
 	<button class="items-center sm:border-none" aria-label={label}>{@render trigger()}</button>
-	<div class="pt-1 z-1 bg-background sm:absolute sm:top-full sm:start-1/2 sm:h-max sm:-translate-x-1/2 sm:rtl:translate-x-1/2 sm:opacity-0 sm:invisible sm:transition-[opacity,visibility] sm:group-hover:opacity-100 sm:group-hover:visible">
+	<div class="pt-1 z-200 bg-background sm:absolute sm:top-full sm:start-1/2 sm:h-max sm:-translate-x-1/2 sm:rtl:translate-x-1/2 sm:opacity-0 sm:invisible sm:transition-[opacity,visibility] sm:group-hover:opacity-100 sm:group-hover:visible">
 		<div class="menu flex flex-col sm:border-2 sm:border-primary sm:shadow-md">
 			{@render children()}
 		</div>
