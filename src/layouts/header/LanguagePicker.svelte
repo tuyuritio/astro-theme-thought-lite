cf97e9555009414bd09e579a8868cbb032d6166c
TuyuriTio <tuyuritio@gmail.com>
2026-05-05 13:13:25 +0900

    fix(i18n): eliminate client-side dynamic i18n hydration

--- src/layouts/header/LanguagePicker.svelte
+++ src/layouts/header/LanguagePicker.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
-import { getRelativeLocaleUrl } from "astro:i18n";
 import { onMount } from "svelte";
+import { SvelteMap } from "svelte/reactivity";
 import config from "$config";
 import i18nit from "$i18n";
 
-let { locale, route }: { locale: string; route: string } = $props();
+let { locale }: { locale: string } = $props();
 
-let path: string | undefined = $derived(route.slice(`/${locale === config.i18n.defaultLocale ? "" : locale}`.length) || undefined);
+type Locales = (typeof config.i18n.locales)[number];
+
+const urls = new SvelteMap<Locales, string>();
+
+function localedURLs() {
+	let links = document.head.querySelectorAll<HTMLAnchorElement>('link[rel="alternate"][hreflang]');
+	links.forEach(link => {
+		let locale = link.getAttribute("hreflang") as Locales;
+		urls.set(locale, link.href);
+	});
+}
 
 onMount(() => {
+	// Initialize the URLs map
+	localedURLs();
+
 	/** Register route update hook */
-	const register = () => window.swup?.hooks.on("page:load", () => (route = window.location.pathname));
+	const register = () => window.swup?.hooks.on("content:replace", localedURLs);
 
 	// Register the hook immediately if swup is already enabled, otherwise wait for the enable event
 	window.swup ? register() : document.addEventListener("swup:enable", register, { once: true });
@@ -18,5 +31,5 @@ onMount(() => {
 </script>
 
 {#each config.i18n.locales as target}
-	<a data-no-swup href={getRelativeLocaleUrl(target, path)} lang={target} aria-current={locale === target ? "page" : undefined} class={locale === target ? "font-bold sm:bg-primary sm:text-background pointer-events-none" : ""}>{i18nit(target)("language")}</a>
+	<a data-no-swup href={urls.get(target)} lang={target} aria-current={locale === target ? "page" : undefined} class={locale === target ? "font-bold sm:bg-primary sm:text-background pointer-events-none" : ""}>{i18nit(target)("language")}</a>
 {/each}
